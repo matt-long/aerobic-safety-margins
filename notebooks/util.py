a3ce72638940a531a333f0640d18d39c25ec75dd
matt-long <mclong@ucar.edu>
2022-02-08 12:21:29 -0700

    woa habitat vol calc

--- notebooks/util.py
+++ notebooks/util.py
@@ -23,6 +23,8 @@ PBS_PROJECT = 'NCGD0011'
 def attrs_label(attrs): 
     """generate a label from long_name and units"""
     name = attrs["long_name"]
+    if len(name) > 30:
+        name = '\n'.join([name[:30], name[30:]])
     units = '' if 'units' not in attrs else f' [{attrs["units"]}]' 
     return name + units
 
@@ -164,13 +166,12 @@ def pop_add_cyclic(ds):
     for v, da in ds.coords.items():
         if not ('nlat' in da.dims and 'nlon' in da.dims):
             dso = dso.assign_coords(**{v: da})
-                
             
     return dso
 
 
 class curator_local_assets(object):    
-    
+    """Curate an intake catalog with locally-cached assets"""
     def __init__(self):
         
         cache_dir = 'data/cache'
@@ -224,4 +225,75 @@ class curator_local_assets(object):
     
     def __repr__(self):
         return self.catalog.__repr__()
+    
+    
+    
+def infer_lat_name(ds):
+    lat_names = ['latitude', 'lat']
+    for n in lat_names:
+        if n in ds:
+            return n
+    raise ValueError('could not determine lat name')    
+
+
+def infer_lon_name(ds):
+    lon_names = ['longitude', 'lon']
+    for n in lon_names:
+        if n in ds:
+            return n
+    raise ValueError('could not determine lon name')       
+
+    
+def lat_weights_regular_grid(lat):
+    """
+    Generate latitude weights for equally spaced (regular) global grids.
+    Weights are computed as sin(lat+dlat/2)-sin(lat-dlat/2) and sum to 2.0.
+    """   
+    dlat = np.abs(np.diff(lat))
+    np.testing.assert_almost_equal(dlat, dlat[0])
+    w = np.abs(np.sin(np.radians(lat + dlat[0] / 2.)) - np.sin(np.radians(lat - dlat[0] / 2.)))
+
+    if np.abs(lat[0]) > 89.9999: 
+        w[0] = np.abs(1. - np.sin(np.radians(np.pi / 2 - dlat[0])))
+
+    if np.abs(lat[-1]) > 89.9999:
+        w[-1] = np.abs(1. - np.sin(np.radians(np.pi / 2 - dlat[0])))
+
+    return w
+
+
+def compute_grid_area(ds, check_total=True):
+    """Compute the area of grid cells.
+    
+    Parameters
+    ----------
+    
+    ds : xarray.Dataset
+      Input dataset with latitude and longitude fields
+    
+    check_total : Boolean, optional
+      Test that total area is equal to area of the sphere.
+      
+    Returns
+    -------
+    
+    area : xarray.DataArray
+       DataArray with area field.
+    
+    """
+    
+    radius_earth = 6.37122e6 # m, radius of Earth
+    area_earth = 4.0 * np.pi * radius_earth**2 # area of earth [m^2]e
+    
+    lon_name = infer_lon_name(ds)       
+    lat_name = infer_lat_name(ds)        
+    
+    weights = lat_weights_regular_grid(ds[lat_name])
+    area = weights + 0.0 * ds[lon_name] # add 'lon' dimension
+    area = (area_earth / area.sum(dim=(lat_name, lon_name))) * area
+    
+    if check_total:
+        np.testing.assert_approx_equal(np.sum(area), area_earth)
+        
+    return xr.DataArray(area, dims=(lat_name, lon_name), attrs={'units': 'm^2', 'long_name': 'area'})  
     
